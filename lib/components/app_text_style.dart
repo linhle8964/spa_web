947e0d88a1a4fa306af72d1e943d74b4feea0a9f
linhlc <le.liam@realestateanalytics.sg>
2023-11-11 12:06:58 +0700

    add text style

---
+++ lib/components/app_text_style.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+import 'package:students/generated/fonts.gen.dart';
+
+class AppTextStyle {
+  static const regular = TextStyle(
+    fontFamily: FontFamily.playfairDisplay,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+    fontSize: 13,
+  );
+
+  static const medium = TextStyle(
+    fontFamily: FontFamily.playfairDisplay,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+
+  static const large = TextStyle(
+    fontFamily: FontFamily.playfairDisplay,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+  );
+
+  static const extraLarge = TextStyle(
+    fontFamily: FontFamily.playfairDisplay,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 32,
+  );
+}
